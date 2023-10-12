7fc4b550ddd34068d5036c2dc1851a3d46fa5430
simon renlund <simon@renlund.fi>
2023-10-12 18:28:45 +0300

    Add base Telegraf project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import "dotenv/config"
+import { Telegraf } from "telegraf"
+
+const bot = new Telegraf(process.env.BOT_TOKEN as string)
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('hipster', Telegraf.reply('λ'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
