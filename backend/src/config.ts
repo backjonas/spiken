a2974ddafd5185336d992497f80dc22b427178c5
simon renlund <simon@renlund.fi>
2023-10-12 19:30:01 +0300

    Add chat restriction

---
+++ backend/src/config.ts
@@ -0,0 +1,12 @@
+import { number, z } from "zod"
+import "dotenv/config"
+
+const processEnvSchema = z.object({
+  BOT_TOKEN: z.string(),
+  CHAT_ID: z.string().transform(val => Number(val))
+})
+const typedProcessEnv = processEnvSchema.parse(process.env)
+export const config = {
+  botToken: typedProcessEnv.BOT_TOKEN,
+  chatId: typedProcessEnv.CHAT_ID,
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,22 @@
-import "dotenv/config"
 import { Telegraf } from "telegraf"
+import { config } from "./config.js"
 
-const bot = new Telegraf(process.env.BOT_TOKEN as string)
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('hipster', Telegraf.reply('λ'))
+const bot = new Telegraf(config.botToken)
+bot.command('patron', async (ctx) => {
+  if (!(await isChatMember(ctx.from.id))) {
+    return ctx.reply("sii dej i reven!")
+  }
+  return ctx.reply('Hello')
+})
 bot.launch()
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+const isChatMember = async (userId: number) => {
+  const acceptedStatuses = ["creator","administrator","member","owner"]
+  const member = await bot.telegram.getChatMember(config.chatId,userId)
+  return acceptedStatuses.includes(member.status)
+}
 
