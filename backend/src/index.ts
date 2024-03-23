e70ca6fe2f3863e32673dc69d46de331bd01943a
christiansegercrantz <44305721+christiansegercrantz@users.noreply.github.com>
2024-03-23 17:32:50 +0000

    Remove unnecessary import

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,6 @@ import {
 import { Message, Update } from '@telegraf/types'
 import { formatDateToString, centsToEuroString } from './utils.js'
 import adminCommands from './admin/index.js'
-import { Product, getProducts } from './products.js'
 import { ContextWithScenes, productsToArray } from './product_handling.js'
 import productCommands from './product_handling.js'
 
